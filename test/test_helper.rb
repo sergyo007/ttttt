350e6a89d8dc8f26e0828920a01f79d23cadda72
sergyo007 <sergyo007@mail.ru>
2017-09-08 17:41:13 +0300

    Finish static pages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,36 @@
-ENV["RAILS_ENV"] ||= "test"
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+#ENV["RAILS_ENV"] ||= "test"
+#require File.expand_path('../../config/environment', __FILE__)
+#require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
+#class ActiveSupport::TestCase
+#  ActiveRecord::Migration.check_pending!
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
-  fixtures :all
+#  fixtures :all
 
   # Add more helper methods to be used by all tests here...
+#end
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'test_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
 end
